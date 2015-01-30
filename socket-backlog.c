eb59c0300e7759b175a61bfd0a41bea1addb4272
laoyang103 <553069938@qq.com>
2015-01-30 15:20:34 +0000

    socket连接队列

---
+++ socket-backlog.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <assert.h>
+#include <sys/signal.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+static int stop = 0;
+
+static void handle_term(int sig)
+{
+    stop = 1;
+}
+
+int main(int argc, char *argv[]) 
+{
+    signal(SIGTERM, handle_term);
+
+    if (argc < 3) {
+        printf("usage: %s ip_address port backlog\n", argv[0]);
+        return 1;
+    }
+
+    const char *ip = argv[1];
+    int port = atoi(argv[2]);
+    int backlog = atoi(argv[3]);
+
+    int sock = socket(PF_INET, SOCK_STREAM, 0);
+    assert(sock >= 0);
+
+    struct sockaddr_in addr;
+    bzero(&addr, sizeof (addr));
+    addr.sin_family = AF_INET;
+    inet_pton(AF_INET, ip, &addr.sin_addr);
+    addr.sin_port = port;
+
+    int ret = bind(sock, (struct sockaddr *)&addr, sizeof (addr));
+    assert(ret != -1);
+
+    ret = listen(sock, backlog);
+    assert(ret != -1);
+
+    while (!stop) {
+        sleep(1);
+    }
+
+    close(sock);
+    return 0;
+}
